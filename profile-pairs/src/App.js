f56044190f1d81470b06a7477fd965a085b51ce0
Jason Ling <jasonling1996@gmail.com>
2023-01-31 17:23:48 -0800

    Add routes to pages

--- profile-pairs/src/App.js
+++ profile-pairs/src/App.js
@@ -1,7 +1,19 @@
+import Dashboard from "./pages/dashboard";
+import Login from "./pages/login";
+import Signup from "./pages/Signup";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-route-dom";
+
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 

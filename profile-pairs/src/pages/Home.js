ee07a57332a70f504fb481d6f79934ebf6610cc3
Jason Ling <jasonling1996@gmail.com>
2023-02-01 15:00:47 -0800

    Add component folder and Navbar file

--- profile-pairs/src/pages/Home.js
+++ profile-pairs/src/pages/Home.js
@@ -1,6 +1,17 @@
 export default function Home() {
 
+  const authToken = true;
+
+  const handleClick = () => {
+    console.log('clicked');
+  }
+
   return (
-    <div>Home</div>
+    <div className="home">
+      <h1>Personality Pairs</h1>
+      <button className="home-button" onClick={handleClick}>
+        {authToken ? "Signout" : "Create Account"}
+      </button>
+    </div>
   )
 };

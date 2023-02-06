faa2eb445b5359eef09f8894df081fa17b2d60ce
Jason Ling <jasonling1996@gmail.com>
2023-02-06 14:22:25 -0800

    Add profile picture section on profile

--- profile-pairs/src/pages/Profile.js
+++ profile-pairs/src/pages/Profile.js
@@ -1,167 +1,127 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Navbar from "../components/Navbar";
 
 export default function Profile() {
+
+  const [formData, setFormData] = useState({
+    user_id: "",
+    first_name: "",
+    dob: "",
+    gender: "",
+    gender_interest: "",
+    email: "",
+    url: "",
+    about: "",
+    matches: []
+  })
 
   const handleSubmit = () => {
     console.log("handle Submit");
   }
 
-  const handleChange = () => {
-    console.log("handle change")
+  const handleChange = (event) => {
+    const value = event.target.value;
+    const name = e.target.name;
+    console.log(`value` + value + `name` + name)
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value
+    }))
   }
 
-  return (
-    <>
-    <Navbar
-      showModal={false}
-      setShowModal={() => {}}
-      />
+  return (<>
+    <Navbar showModal={false}
+      setShowModal={
+        () => {}
+      }/>
     <div className="profile">
       <h2>Create Profile</h2>
 
       <form onSubmit={handleSubmit}>
-      <section>
-        <label htmlFor="first_name">First Name</label>
-        <input
-          id="first_name"
-          type="text"
-          name="first_name"
-          placeholder="First Name"
-          required={true}
-          value={""}
-          onChange={handleChange}
-          />
+        <section>
+          <label htmlFor="first_name">First Name</label>
+          <input id="first_name" type="text" name="first_name" placeholder="First Name"
+            required={true}
+            value={formData.first_name}
+            onChange={handleChange}/>
 
-        <label>Last Name</label>
-        <input
-          id="last_name"
-          type="text"
-          name="last_name"
-          placeholder="Last Name"
-          required={true}
-          value={""}
-          onChange={handleChange}
-          />
+          <label>Last Name</label>
+          <input id="last_name" type="text" name="last_name" placeholder="Last Name"
+            required={true}
+            value={formData.last_name}
+            onChange={handleChange}/>
 
-        <label>Date of Birth</label>
-        <input
-          id="dob"
-          type="text"
-          name="dob"
-          placeholder="Date of Birth"
-          required={true}
-          value={""}
-          onChange={handleChange}
-          />
+          <label>Date of Birth</label>
+          <input id="dob" type="text" name="dob" placeholder="Date of Birth"
+            required={true}
+            value={formData.dob}
+            onChange={handleChange}/>
 
-        <label>Gender</label>
-        <div className="multiple-input">
-          <input
-            id="male"
-            type="radio"
-            name="gender"
-            value={"male"}
-            checked={false}
-            onChange={handleChange}
-            />
+          <label>Gender</label>
+          <div className="multiple-input">
+            <input id="male" type="radio" name="gender"
+              value={"male"}
+              checked={false}
+              onChange={handleChange}/>
             <label htmlFor="male-gender">Male</label>
-            <input
-            id="female"
-            type="radio"
-            name="gender"
-            value={"female"}
-            checked={false}
-            onChange={handleChange}
-            />
+            <input id="female" type="radio" name="gender"
+              value={"female"}
+              checked={false}
+              onChange={handleChange}/>
             <label htmlFor="female-gender">Female</label>
-            <input
-            id="LGBTQ2Q+"
-            type="radio"
-            name="gender"
-            value={"LGBTQ2Q+"}
-            checked={false}
-            onChange={handleChange}
-            />
+            <input id="LGBTQ2Q+" type="radio" name="gender"
+              value={"LGBTQ2Q+"}
+              checked={false}
+              onChange={handleChange}/>
             <label htmlFor="LGBTQ2Q+">LGBTQ2Q+</label>
           </div>
 
           <label>Show Me</label>
           <div className="multiple-input-container">
-          <input
-            id="male_interest"
-            type="radio"
-            name="gender"
-            value={"male"}
-            checked={false}
-            onChange={handleChange}
-            />
+            <input id="male_interest" type="radio" name="gender"
+              value={"male"}
+              checked={false}
+              onChange={handleChange}/>
             <label htmlFor="male-interest">Male</label>
-            <input
-            id="female_interest"
-            type="radio"
-            name="gender"
-            value={"female"}
-            checked={false}
-            onChange={handleChange}
-            />
+            <input id="female_interest" type="radio" name="gender"
+              value={"female"}
+              checked={false}
+              onChange={handleChange}/>
             <label htmlFor="female-interest">Female</label>
-            <input
-            id="LGBTQ2Q+_interest"
-            type="radio"
-            name="gender"
-            value={"LGBTQ2Q+"}
-            checked={false}
-            onChange={handleChange}
-            />
+            <input id="LGBTQ2Q+_interest" type="radio" name="gender"
+              value={"LGBTQ2Q+"}
+              checked={false}
+              onChange={handleChange}/>
             <label htmlFor="LGBTQ2Q+-interest">LGBTQ2Q+</label>
           </div>
 
           <label htmlFor="about">About Me</label>
-          <input
-            id="about"
-            type="text"
-            name="about"
+          <input id="about" type="text" name="about"
             required={true}
             placeholder="Anything interesting you would like others to know"
-            value={""}
-            onChange={handleChange}
-          />
+            value={formData.about}
+            onChange={handleChange}/>
           <input type="submit"/>
-      </section>
+        </section>
 
-      <section>
+        <section>
+          <label htmlFor="profile_pics">Profile Pictures #1</label>
+          <input type="url" name="pp" id="pp1"
+            onChange={handleChange}
+            required={true}/>
+          <label htmlFor="profile_pics">Profile Pictures #2</label>
+          <input type="url" name="pp" id="pp2"
+            onChange={handleChange}
+            required={true}/>
+          <label htmlFor="profile_pics">Profile Pictures #3</label>
+          <input type="url" name="pp" id="pp3"
+            onChange={handleChange}
+            required={true}/>
 
-      <label htmlFor="profile_pics">Profile Pictures #1</label>
-      <input
-        type="url"
-        name="pp"
-        id="pp1"
-        onChange={handleChange}
-        required={true}
-      />
-    <label htmlFor="profile_pics">Profile Pictures #2</label>
-      <input
-        type="url"
-        name="pp"
-        id="pp2"
-        onChange={handleChange}
-        required={true}
-      />
-    <label htmlFor="profile_pics">Profile Pictures #3</label>
-      <input
-        type="url"
-        name="pp"
-        id="pp3"
-        onChange={handleChange}
-        required={true}
-      />
-        <div className="photo-container">
-
-        </div>
-      </section>
+          <div className="photo-container"></div>
+            <img src={formData.url} alt="profile pic preview"/>
+        </section>
       </form>
     </div>
-    </>
-  )
+  </>)
 };

abde9b8830696f2d640f568ecf79d3be5399709b
Jason Ling <jasonling1996@gmail.com>
2023-02-08 15:12:55 -0800

    Add npm package TinderCard

--- profile-pairs/src/pages/Dashboard.js
+++ profile-pairs/src/pages/Dashboard.js
@@ -1,6 +1,14 @@
+import TinderCard from "react-tinder-card"
+
 export default function Dashboard() {
 
   return (
-    <div>Dashboard</div>
+    <div className="dashboard">
+      <div className="swiper-container">
+        <div className="card-container">
+
+        </div>
+      </div>
+    </div>
   )
 };

152de181a9712c18860bc9737980b99218e55be8
Jason Ling <jasonling1996@gmail.com>
2023-03-09 11:11:45 -0800

    Add new messages

--- client/src/components/ChatInput.js
+++ client/src/components/ChatInput.js
@@ -1,7 +1,29 @@
 import { useState } from "react"
+import axios from "axios"
 
 export default function ChatInput(props) {
   const [textArea, setTextArea] = useState(null)
+  const userId = props.user?.user_id
+  const clickUserId = props.clickedUser?.user_id
+
+  const addMessage = async () => {
+    const message = {
+      timestamp: new Date().toISOString,
+      from_userId: userId,
+      to_userId: clickUserId,
+      message: textArea
+    }
+
+    try {
+      await axios.post('http://localhost:8000/message', { message })
+      props.getUsersMessages()
+      props.getClickedUsersMessages()
+      setTextArea("")
+    } catch(err) {
+      console.log(err.message)
+    }
+  }
+
   return (
     <div className="chat-input-container">
       <textarea value={textArea} onChange ={(event) => setTextArea(event.target.value)}/>

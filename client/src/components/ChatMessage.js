78d1b27e8e0ad274fc8f9652e5926fa97de133e0
Jason Ling <jasonling1996@gmail.com>
2023-03-08 23:34:51 -0800

    Add user messages into chat message

--- client/src/components/ChatMessage.js
+++ client/src/components/ChatMessage.js
@@ -1,14 +1,28 @@
 import ChatInput from "./ChatInput"
 import Chat from "./Chat"
 import axios from "axios"
+import { useEffect, useState } from "react"
 
 export default function ChatMessage(props) {
+  const [userMessages, setUsersMessages] = useState(null)
+  const userId = props.user?.user_id
+  const clickedUserId = props.clickedUser?.user_id
+
 
   const getUsersMessages = async () => {
-    const response = await axios.get('http://localhost8000:/messages', {
+    try {
+      const response = await axios.get('http://localhost8000:/messages', {
       params: {userId: userId, correspondingUserId: clickedUserId}
     })
+    setUsersMessages(response.data)
+    }catch (err) {
+      console.log(err.message)
+    }
   }
+
+  useEffect(() => {
+    getUsersMessages()
+  }, [userMessages])
 
   return (
     <>

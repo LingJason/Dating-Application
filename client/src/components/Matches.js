6fa96154f4d054d8696e8071144e927343c665bc
Jason Ling <jasonling1996@gmail.com>
2023-03-09 11:47:09 -0800

    Fixed Errors

--- client/src/components/Matches.js
+++ client/src/components/Matches.js
@@ -1,37 +1,47 @@
-import axios from 'axios';
-import {useEffect, useState} from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 
 export default function Matches(props) {
-  const [matchedProfiles, setMatchedProfiles] = useState(null)
+  const [matchedProfiles, setMatchedProfiles] = useState(null);
+  const [cookies, setCookies, removeCookies] = useCookies(null);
 
-  const matchedUserIds = props.matches.map(({ user_id }) => user_id)
+  const matchedUserIds = props.matches.map(({ user_id }) => user_id);
+  const userId = cookies.UserId;
 
-  const getMatches = async() => {
+  const getMatches = async () => {
     try {
-      const response = await axios.get('http://localhost:8000/users', {
-        params: {userIds: JSON.stringify(matchedUserIds)}
-      })
-      setMatchedProfiles(response.data)
+      const response = await axios.get("http://localhost:8000/users", {
+        params: { userIds: JSON.stringify(matchedUserIds) },
+      });
+      setMatchedProfiles(response.data);
     } catch (err) {
-      console.log(err.message)
+      console.log(err.message);
     }
-  }
+  };
 
   useEffect(() => {
-    getMatches()
-  }, [props.matches])
+    getMatches();
+  }, [props.matches]);
+
+  const filteredMatchesProfiles = matchedProfiles?.filter((matchedProfile) =>
+    matchedProfile.props.matches.filter((profile) => profile.user_id == userId)/length > 0
+  );
 
   return (
     <div className="matches-container">
-      {matchedProfiles?.map((match) => (
-        <div key={match.user_id} className="match-card" onClick={() => props.setClickedUser(match)}>
-          <div className='img-container'>
-            <img src={match?.url} alt={match?.first_name + 'profile'}/>
+      {filteredMatchesProfiles?.map((match, _index) => (
+        <div
+          key={_index}
+          className="match-card"
+          onClick={() => props.setClickedUser(match)}
+        >
+          <div className="img-container">
+            <img src={match?.url} alt={match?.first_name + "profile"} />
           </div>
           <h3>{match?.first_name}</h3>
         </div>
       ))}
-
     </div>
-  )
-};
+  );
+}

d1e4a58d9ee8ec7e9e0ecd71be24fd7b52d9c4c0
Jason Ling <jasonling1996@gmail.com>
2023-03-09 11:26:07 -0800

    Fixed error for textArea

--- client/src/components/ChatInput.js
+++ client/src/components/ChatInput.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 import axios from "axios"
 
 export default function ChatInput(props) {
-  const [textArea, setTextArea] = useState(null)
+  const [textArea, setTextArea] = useState("")
   const userId = props.user?.user_id
   const clickUserId = props.clickedUser?.user_id
 

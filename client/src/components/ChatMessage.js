8e632f36e6d5d74f5def4c6e747a285b4e5d6416
Jason Ling <jasonling1996@gmail.com>
2023-03-09 11:20:50 -0800

    Add backend route for message

--- client/src/components/ChatMessage.js
+++ client/src/components/ChatMessage.js
@@ -1,69 +1,74 @@
-import ChatInput from "./ChatInput"
-import Chat from "./Chat"
-import axios from "axios"
-import { useEffect, useState } from "react"
+import ChatInput from "./ChatInput";
+import Chat from "./Chat";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function ChatMessage(props) {
-  const [usersMessages, setUsersMessages] = useState(null)
-  const [clickedUsersMessages, setClickedUsersMessages] = useState(null)
-  const userId = props.user?.user_id
-  const clickedUserId = props.clickedUser?.user_id
-
+  const [usersMessages, setUsersMessages] = useState(null);
+  const [clickedUsersMessages, setClickedUsersMessages] = useState(null);
+  const userId = props.user?.user_id;
+  const clickedUserId = props.clickedUser?.user_id;
 
   const getUsersMessages = async () => {
     try {
-      const response = await axios.get('http://localhost8000:/messages', {
-      params: {userId: userId, correspondingUserId: clickedUserId}
-    })
-    setUsersMessages(response.data)
-    }catch (err) {
-      console.log(err.message)
+      const response = await axios.get("http://localhost8000:/messages", {
+        params: { userId: userId, correspondingUserId: clickedUserId },
+      });
+      setUsersMessages(response.data);
+    } catch (err) {
+      console.log(err.message);
     }
-  }
+  };
 
   const getClickedUsersMessages = async () => {
     try {
-      const response = await axios.get('http://localhost8000:/messages', {
-      params: {userId: clickedUserId, correspondingUserId: userId}
-    })
-    setClickedUsersMessages(response.data)
-    }catch (err) {
-      console.log(err.message)
+      const response = await axios.get("http://localhost8000:/messages", {
+        params: { userId: clickedUserId, correspondingUserId: userId },
+      });
+      setClickedUsersMessages(response.data);
+    } catch (err) {
+      console.log(err.message);
     }
-  }
-
+  };
 
   useEffect(() => {
-    getUsersMessages()
-    getClickedUsersMessages()
-  }, [usersMessages, clickedUsersMessages])
+    getUsersMessages();
+    getClickedUsersMessages();
+  }, [usersMessages, clickedUsersMessages]);
 
-  const messages = []
+  const messages = [];
 
-  usersMessages?.forEach(message => {
-    const formattedMessage = {}
-    formattedMessage['name'] = user?.first_name
-    formattedMessage['img'] = user?.url
-    formattedMessage['message'] = message.message
-    formattedMessage['timestamp'] = message.timestamp
-    message.push(formattedMessage)
-  }) 
+  usersMessages?.forEach((message) => {
+    const formattedMessage = {};
+    formattedMessage["name"] = user?.first_name;
+    formattedMessage["img"] = user?.url;
+    formattedMessage["message"] = message.message;
+    formattedMessage["timestamp"] = message.timestamp;
+    message.push(formattedMessage);
+  });
 
-  clickedUsersMessages?.forEach(message => {
-    const formattedMessage = {}
-    formattedMessage['name'] = clickedUser?.first_name
-    formattedMessage['img'] = clickedUser?.url
-    formattedMessage['message'] = message.message
-    formattedMessage['timestamp'] = message.timestamp
-    message.push(formattedMessage)
-  }) 
+  clickedUsersMessages?.forEach((message) => {
+    const formattedMessage = {};
+    formattedMessage["name"] = clickedUser?.first_name;
+    formattedMessage["img"] = clickedUser?.url;
+    formattedMessage["message"] = message.message;
+    formattedMessage["timestamp"] = message.timestamp;
+    message.push(formattedMessage);
+  });
 
-  const descendingOrderMessages = messages?.sort((a,b) => a.timestamp.localeCompare(b.timestamp))
+  const descendingOrderMessages = messages?.sort((a, b) =>
+    a.timestamp.localeCompare(b.timestamp)
+  );
 
   return (
     <>
-    <Chat descendingOrderMessages={descendingOrderMessages}/>
-    <ChatInput/>
+      <Chat descendingOrderMessages={descendingOrderMessages} />
+      <ChatInput
+        user={user}
+        clickedUser={clickedUsers}
+        getUsersMessages={getUsersMessages}
+        getClickedUsersMessages={getClickedUsersMessages}
+      />
     </>
-  )
-};
+  );
+}

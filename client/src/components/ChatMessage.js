5dcb055e946cd902e0dcb35a6823bec2c3fad14d
Jason Ling <jasonling1996@gmail.com>
2023-03-09 09:35:35 -0800

    Display user and clickedUser chat

--- client/src/components/ChatMessage.js
+++ client/src/components/ChatMessage.js
@@ -4,7 +4,8 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 
 export default function ChatMessage(props) {
-  const [userMessages, setUsersMessages] = useState(null)
+  const [usersMessages, setUsersMessages] = useState(null)
+  const [clickedUsersMessages, setClickedUsersMessages] = useState(null)
   const userId = props.user?.user_id
   const clickedUserId = props.clickedUser?.user_id
 
@@ -20,13 +21,48 @@ export default function ChatMessage(props) {
     }
   }
 
+  const getClickedUsersMessages = async () => {
+    try {
+      const response = await axios.get('http://localhost8000:/messages', {
+      params: {userId: clickedUserId, correspondingUserId: userId}
+    })
+    setClickedUsersMessages(response.data)
+    }catch (err) {
+      console.log(err.message)
+    }
+  }
+
+
   useEffect(() => {
     getUsersMessages()
-  }, [userMessages])
+    getClickedUsersMessages()
+  }, [usersMessages, clickedUsersMessages])
+
+  const messages = []
+
+  usersMessages?.forEach(message => {
+    const formattedMessage = {}
+    formattedMessage['name'] = user?.first_name
+    formattedMessage['img'] = user?.url
+    formattedMessage['message'] = message.message
+    formattedMessage['timestamp'] = message.timestamp
+    message.push(formattedMessage)
+  }) 
+
+  clickedUsersMessages?.forEach(message => {
+    const formattedMessage = {}
+    formattedMessage['name'] = clickedUser?.first_name
+    formattedMessage['img'] = clickedUser?.url
+    formattedMessage['message'] = message.message
+    formattedMessage['timestamp'] = message.timestamp
+    message.push(formattedMessage)
+  }) 
+
+  const descendingOrderMessages = messages?.sort((a,b) => a.timestamp.localeCompare(b.timestamp))
 
   return (
     <>
-    <Chat/>
+    <Chat descendingOrderMessages={descendingOrderMessages}/>
     <ChatInput/>
     </>
   )
